50c2f68edefa194a982251f22a16dd213977e75d
July Palomiano <13739471+julzpalomiano@users.noreply.github.com>
2023-10-01 20:47:51 +0800

    setup

---
+++ app/Filament/Resources/DeparmentResource/Pages/CreateDeparment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\DeparmentResource\Pages;
+
+use App\Filament\Resources\DeparmentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDeparment extends CreateRecord
+{
+    protected static string $resource = DeparmentResource::class;
+}
